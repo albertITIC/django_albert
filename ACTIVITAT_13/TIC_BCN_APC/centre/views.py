6118bb1a5a6b5a2979f80059f34abbd9fab53a63
Albert <albertpencas@gmail.com>
2025-01-29 20:50:40 +0100

    Bucle afegit i header

--- ACTIVITAT_13/TIC_BCN_APC/centre/views.py
+++ ACTIVITAT_13/TIC_BCN_APC/centre/views.py
@@ -6,46 +6,38 @@ from django.template import loader # Importem el template
 # Create your views here.
 
 def alumne(request):
-    alumne = {
-        "name":"Albert",
-        "surname1":"Penadés",
-        "surname2":"Casajús", 
-        "email":"albert@gmail.com", 
-        "curse":"DAW2A", 
-        "modules":"M6, M7, M8, M9" 
-    }
+    alumnes = [
+        {
+            # Exemple 1
+            "name":"Albert","surname1":"Penadés","surname2":"Casajús","email":"albert@gmail.com","curse":"DAW2A","modules":"M6, M7, M8, ML" 
+        },
+        {
+            # Exemple 2
+            "name":"Víctor","surname1":"Fernández","surname2":"Álvarez","email":"victor@gmail.com","curse":"DAW2A","modules":"M6, M7, M8, IA" 
+        },
+        {
+            # Exemple 3
+            "name":"ejemplo","surname1":"ejemplo","surname2":"ejemplo","email":"ejemplo@gmail.com","curse":"DAW2A","modules":"ejemplo" 
+        },
+        
+    ]
     
     template = loader.get_template('alumnes_centre.html')
     
-    dades = template.render({
-        'nom':alumne["name"],
-        'cognom1':alumne["surname1"],
-        'cognom2':alumne["surname2"],
-        'correu':alumne["email"],
-        'curs':alumne["curse"],
-        'moduls':alumne["modules"]  
-    })
+    dades = template.render({'alumnes':alumnes})
     
     return HttpResponse(dades)
 
 # Per professors
 def professor(request):
-    professor = [{
+    professor = {
         "name":"Roger",
         "surname1":"Sobrino",
         "surname2":"Gil", 
         "email":"roger@gmail.com", 
         "curse":"DAW2A", 
         "modules":"M6, M13" 
-    }, 
-    {
-        "name": "Laura",
-        "surname1": "Gómez",
-        "surname2": "López",
-        "email": "laura.gomez@gmail.com",
-        "department": "Matemàtiques",
-        "subjects": ["Àlgebra", "Càlcul"]
-    }]
+    }
     
     template = loader.get_template('professors_centre.html')
     
@@ -55,7 +47,8 @@ def professor(request):
         'cognom2':professor["surname2"],
         'correu':professor["email"],
         'curs':professor["curse"],
-        'moduls':professor["modules"]
+        'moduls':professor["modules"],
+        
         })
     
     return HttpResponse(dades)
@@ -64,8 +57,6 @@ def professor(request):
 # def index(request):
 #     alumne = {{"name":"Albert", "surname":"Penades", "age":21}}
 #     return render(request, 'index_centre.html', {"name":alumne["name"], "surname":alumne["surname"], "age":alumne["age"]})
-
-
 
 # def students(request):
 #     template = loader.get_template('index_centre.html')

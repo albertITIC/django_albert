27e1b2d3a28008180a9a890e4831fd81718b8689
Albert <albertpencas@gmail.com>
2025-02-02 19:36:50 +0100

    Punt 1 finalitzat, canvis al css, header complementat, més exemples d'alumnes i professors

--- ACTIVITAT_13/TIC_BCN_APC/centre/views.py
+++ ACTIVITAT_13/TIC_BCN_APC/centre/views.py
@@ -9,17 +9,43 @@ def alumne(request):
     alumnes = [
         {
             # Exemple 1
-            "name":"Albert","surname1":"Penadés","surname2":"Casajús","email":"albert@gmail.com","curse":"DAW2A","modules":"M6, M7, M8, ML" 
+            "name":"Albert","surname1":"Penadés","surname2":"Casajús","email":"2023_albert.penades@iticbcn.cat","curse":"DAW2A","modules":"M6, M7, M8, ML" 
         },
         {
             # Exemple 2
-            "name":"Víctor","surname1":"Fernández","surname2":"Álvarez","email":"victor@gmail.com","curse":"DAW2A","modules":"M6, M7, M8, IA" 
+            "name":"Víctor A.","surname1":"Fernández","surname2":"Álvarez","email":"2023_victor.fernandez@iticbcn.cat","curse":"DAW2A","modules":"M06, M07, M08, IA" 
         },
         {
             # Exemple 3
-            "name":"ejemplo","surname1":"ejemplo","surname2":"ejemplo","email":"ejemplo@gmail.com","curse":"DAW2A","modules":"ejemplo" 
+            "name":"Hugo","surname1":"Sansegundo","surname2":"Costantini","email":"2023_hugo.sansegundo@iticbcn.cat","curse":"DAW2A","modules":"M06, M07, M08, ML" 
         },
-        
+        {
+            "name":"Adrián","surname1":"Navarro","surname2":"Pérez","email":"2023_adrian.navarro@iticbcn.cat","curse":"DAW2A","modules":"M06, M07, M08, Cloud"    
+        },
+        {
+            "name":"Xavi","surname1":"Porras","surname2":"del Pino","email":"2023_xavier.porras@iticbcn.cat","curse":"DAW2A","modules":"M06, M07, M08"    
+        },
+        {
+            "name":"Javier","surname1":"Giménez","surname2":"Sánchez","email":"2023_javier.gimenez@iticbcn.cat","curse":"DAW2A","modules":"M06, M07, M08, ML"    
+        },
+        {
+            "name":"Milena","surname1":"Vardumyan","surname2":"Aleksanyan","email":"2023_milena.vardumyan@iticbcn.cat","curse":"DAW2A","modules":"M06, M07, M013, Big Data"    
+        },
+        {
+            "name":"Daniel","surname1":"Vallespin","surname2":"Mellado","email":"2023_daniel.vallespin@iticbcn.cat","curse":"DAW2A","modules":"M06, M07, M08"    
+        },
+        {
+            "name":"Félix Bequet","surname1":"Balbin","surname2":"Silva","email":"2023_felix.balbin@iticbcn.cat","curse":"DAW2A","modules":"M06, M07, M08, Big Data"    
+        },
+        {
+            "name":"Lila","surname1":"Díez","surname2":"Zhou","email":"2023_lila.diez@iticbcn.cat","curse":"DAW2A","modules":"M06,M07,M08, ML"    
+        },
+        {
+            "name":"Arman","surname1":"Mohammed","surname2":"Akther","email":"2023_arman.mohammed@iticbcn.cat","curse":"DAW2A","modules":"M06, M07, M08"    
+        },
+        {
+            "name":"Natalia","surname1":"Casanellas","surname2":"Blanquer","email":"2023_natalia.casanellas@iticbcn.cat","curse":"DAW2A","modules":"M06, M07"    
+        }, 
     ]
     
     template = loader.get_template('alumnes_centre.html')
@@ -30,34 +56,24 @@ def alumne(request):
 
 # Per professors
 def professor(request):
-    professor = {
-        "name":"Roger",
-        "surname1":"Sobrino",
-        "surname2":"Gil", 
-        "email":"roger@gmail.com", 
-        "curse":"DAW2A", 
-        "modules":"M6, M13" 
-    }
     
-    template = loader.get_template('professors_centre.html')
+    professors = [
+        {
+            "name":"Jordi", "surname1":"Quesada", "email":"jordi@gmail.com", "curse":"DAW2A", "modules":"M03"
+        },
+        {
+            "name":"Juanma", "surname1":"Sànchez", "email":"juanma@gmail.com", "curse":"DAW2A", "modules":"M08"
+        },
+        {
+            "name":"Roger", "surname1":"Sobrino", "email":"roger@gmail.com", "curse":"DAW2A", "modules":"M07"
+        },
+        {
+            "name":"Oriol", "surname1":"Roca", "email":"oriol@gmail.com", "curse":"DAW2A", "modules":"M06"
+        },
+    ]  
     
-    dades = template.render({
-        'nom':professor["name"],
-        'cognom1':professor["surname1"],
-        'cognom2':professor["surname2"],
-        'correu':professor["email"],
-        'curs':professor["curse"],
-        'moduls':professor["modules"],
-        
-        })
-    
-    return HttpResponse(dades)
+    template = loader.get_template('professors_centre.html')    
 
-# Versió resumida
-# def index(request):
-#     alumne = {{"name":"Albert", "surname":"Penades", "age":21}}
-#     return render(request, 'index_centre.html', {"name":alumne["name"], "surname":alumne["surname"], "age":alumne["age"]})
+    dades2 = template.render({'professors':professors})
 
-# def students(request):
-#     template = loader.get_template('index_centre.html')
-#     return HttpResponse(template.render())
+    return HttpResponse(dades2)
